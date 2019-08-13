689dbdbd54122d26f8629872d4b9a7790c58034f
larissacsilva <larissa.do.c.silva@gmail.com>
2019-08-13 11:23:10 -0400

    Adicionando o metodo do Funcionario

--- confeitaria-domain/src/confeitaria/domain/Main.java
+++ confeitaria-domain/src/confeitaria/domain/Main.java
@@ -1,6 +1,70 @@
-
 package confeitaria.domain;
 
+import confeitaria.domain.dao.IFuncionarioDAO;
+import confeitaria.domain.dao.list.FuncionarioDAOImpl;
+import confeitaria.domain.entidades.Funcionario;
+import java.util.Scanner;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+        IFuncionarioDAO banco = new FuncionarioDAOImpl();
+        Scanner t = new Scanner(System.in);
+        int opcao = 0;
+        do {
+            System.out.println("Menu:");
+            System.out.println("1 - Cadastrar");
+            System.out.println("2 - Atualizar");
+            System.out.println("3 - Remover");
+            System.out.println("4 - Consultar");
+            System.out.println("5 - Sair");
+            System.out.println("Digite a opção:");
+            opcao = t.nextInt();
+            
+            switch(opcao){
+                case 1:
+                    System.out.println("Cadastrando.....");
+                    Funcionario nova = new Funcionario();
+                    System.out.println("Digite o nome:");
+                    t.nextLine();
+                    nova.setNome(t.nextLine());
+                    try{
+                        banco.cadastrar(nova);
+                        System.out.println("Inserido com Sucesso!");
+                    }catch(Exception erro){
+                        System.out.println(erro.getMessage());
+                    }
+                    break;
+                case 2:
+                    System.out.println("Atualizando...");
+                    System.out.println("Digite o ID:");
+                    Funcionario c = new Funcionario();
+                    c.setCpf(t.nextInt());
+                    System.out.println("Digite o nome atualizado:");
+                    c.setNome(t.next());
+                    banco.alterar(c);
+                    System.out.println("Atualizado com Sucesso!");
+                    break;
+                case 3:
+                    System.out.println("Removendo...");
+                    System.out.println("Digite o ID:");
+                    banco.excluir(t.nextInt());
+                    System.out.println("Removido com Sucesso!");
+                    break;
+                case 4:
+                    System.out.println("Listando...");
+                    for(Funcionario ent : banco.consultar()){
+                        System.out.println(ent.getCpf() + " - " + ent.getNome());
+                    }
+                    break;
+                case 5:
+                    break;
+                default:
+                    System.out.println("Opção Inválida!");
+            }
+            
+        } while (opcao != 5);
+
+    }
+
 }
---
+++ confeitaria-domain/src/confeitaria/domain/dao/list/FuncionarioDAOImpl.java
@@ -0,0 +1,52 @@
+package confeitaria.domain.dao.list;
+
+import confeitaria.domain.dao.IFuncionarioDAO;
+import confeitaria.domain.entidades.Funcionario;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FuncionarioDAOImpl implements IFuncionarioDAO{
+    
+    private static List<Funcionario> funcionarios = new ArrayList<>();
+    private static int lastId = 1;
+
+    public void cadastrar(Funcionario ent) {
+        if(ent == null){
+            throw new NullPointerException();
+        }
+        if(funcionarios.contains(ent)){
+            throw new RuntimeException("Valor repetido!");
+        }
+        ent.setCpf(lastId);
+        lastId++;
+        funcionarios.add(ent);
+    }
+
+    public void alterar(Funcionario ent) {
+        for (int i = 0; i < funcionarios.size(); i++) {
+            Funcionario get = funcionarios.get(i);
+            if(ent.getCpf() == get.getCpf()){
+                if(!funcionarios.contains(ent)){
+                    funcionarios.set(i, ent);
+                }
+            }
+        }
+    }
+
+    public void excluir(int id) {
+        for (int i = 0; i < funcionarios.size(); i++) {
+            Funcionario get = funcionarios.get(i);
+            if(get.getCpf() == id){
+                funcionarios.remove(i);
+                break;
+            }
+        }
+    }
+
+    public List<Funcionario> consultar() {
+        return funcionarios;
+    }
+
+
+    
+}
